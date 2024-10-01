bf21406bcc668ee43b485d9955a9ac997e001871
Sito <sitopatrick2004@gmail.com>
2024-10-01 14:50:42 -0700

    Initial commit

---
+++ Assets/Challenge 5/Scripts/CountdownTimer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    // public float totalTime = 60;
+    // public TextMeshPro timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(totalTime > 0) {
+        //     totalTime -= Time.deltaTime;
+        //     float minutes = Mathf.FloortoInt(totalTime / 60);
+        //     float seconds = Mathf.FloortoInt(totalTime % 60);
+        //     timerText.text = string.Format("{0:00}: {1:00}", minutes, seconds);
+        // }
+
+        // else {
+        //     totalTime = 0;
+        // }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Whack_A_Food by Sito
+ This is my submission for week 5 challenge on GameUp Africa Bootcamp
